8eb67fb1abd078f2a13e08fc5d9b77a3d0abdf32
damiano.calcagni <damiano.calcagni@king.com>
2019-02-14 22:52:33 +0100

    Adds project dependencies and first code implementation
    - Adds OpenGL dependency
    - Adds GLUT dependency
    - Implement window setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,22 @@
-int main() 
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h>
+
+void Render()
 {
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(1024, 768);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+	glutCreateWindow("ERM");
+	
+	glutDisplayFunc(Render);
+	
+	glutMainLoop();
+	
 	return 0;
 }
