f82f0c7f32513bd2a3b52c85f5883cf5f37e41cd
JALB91 <damiano.calcagni@gmail.com>
2019-06-13 23:03:03 +0200

    Fixes MouseButton and Key declaration and usage

--- include/input/Mouse.h
+++ include/input/Mouse.h
@@ -2,17 +2,17 @@
 
 namespace erm {
 	
-	typedef const int MouseButton;
-	extern MouseButton MOUSE_BUTTON_1;
-	extern MouseButton MOUSE_BUTTON_2;
-	extern MouseButton MOUSE_BUTTON_3;
-	extern MouseButton MOUSE_BUTTON_4;
-	extern MouseButton MOUSE_BUTTON_5;
-	extern MouseButton MOUSE_BUTTON_6;
-	extern MouseButton MOUSE_BUTTON_7;
-	extern MouseButton MOUSE_BUTTON_8;
-	extern MouseButton MOUSE_BUTTON_LEFT;
-	extern MouseButton MOUSE_BUTTON_RIGHT;
-	extern MouseButton MOUSE_BUTTON_MIDDLE;
+	typedef int MouseButton;
+	extern const MouseButton MOUSE_BUTTON_1;
+	extern const MouseButton MOUSE_BUTTON_2;
+	extern const MouseButton MOUSE_BUTTON_3;
+	extern const MouseButton MOUSE_BUTTON_4;
+	extern const MouseButton MOUSE_BUTTON_5;
+	extern const MouseButton MOUSE_BUTTON_6;
+	extern const MouseButton MOUSE_BUTTON_7;
+	extern const MouseButton MOUSE_BUTTON_8;
+	extern const MouseButton MOUSE_BUTTON_LEFT;
+	extern const MouseButton MOUSE_BUTTON_RIGHT;
+	extern const MouseButton MOUSE_BUTTON_MIDDLE;
 	
 }
