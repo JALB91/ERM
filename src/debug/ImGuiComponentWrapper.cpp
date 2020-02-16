0d3b5f050d274d06b154972a98a7fe2d715d93f9
damiano.calcagni <damiano.calcagni@gmail.com>
2020-02-16 20:16:26 +0100

    Adds the possibility of adding components to entities

--- src/debug/ImGuiComponentWrapper.cpp
+++ src/debug/ImGuiComponentWrapper.cpp
@@ -5,7 +5,10 @@
 
 #include "game/Game.h"
 
+#include "window/Window.h"
+
 #include "ecs/ECS.h"
+#include "ecs/Entity.h"
 #include "ecs/EntityId.h"
 #include "ecs/components/TransformComponent.h"
 #include "ecs/components/ModelComponent.h"
@@ -14,6 +17,8 @@
 #include "ecs/systems/ModelSystem.h"
 #include "ecs/systems/CameraSystem.h"
 
+#include <imgui.h>
+
 namespace ImGui {
 	
 	void ShowComponentDebugWindow(erm::ecs::EntityId entity)
@@ -21,18 +26,64 @@ namespace ImGui {
 		if (!entity.IsValid()) return;
 		
 		erm::Game& game = erm::Game::GetInstance();
+		bool hasTransform = false;
+		bool hasModel = false;
+		bool hasCamera = false;
 		
 		if (erm::ecs::TransformComponent* transformComponent = game.GetECS().GetSystem<erm::ecs::TransformSystem>().GetComponent(entity()))
 		{
 			ShowTransformComponentDebugWindow(*transformComponent);
+			hasTransform = true;
 		}
 		if (erm::ecs::ModelComponent* modelComponent = game.GetECS().GetSystem<erm::ecs::ModelSystem>().GetComponent(entity()))
 		{
 			ShowModelComponentDebugWindow(*modelComponent);
+			hasModel = true;
 		}
 		if (erm::ecs::CameraComponent* cameraComponent = game.GetECS().GetSystem<erm::ecs::CameraSystem>().GetComponent(entity()))
 		{
 			ShowCameraComponentDebugWindow(*cameraComponent);
+			hasCamera = true;
+		}
+		
+		ImGui::Separator();
+		
+		const bool hasMissingComponents = (!hasTransform || !hasModel || !hasCamera);
+		
+		if (hasMissingComponents && ImGui::Button("Add Component..."))
+		{
+			ImGui::OpenPopup("Components");
+		}
+		
+		if (hasMissingComponents && ImGui::BeginPopup("Components"))
+		{
+			if (!hasTransform)
+			{
+				if (ImGui::Button("Transform"))
+				{
+					game.GetECS().GetEntityById(entity)->AddComponent<erm::ecs::TransformComponent>();
+					ImGui::CloseCurrentPopup();
+				}
+			}
+			
+			if (!hasModel)
+			{
+				if (ImGui::Button("Model"))
+				{
+					game.GetECS().GetEntityById(entity)->AddComponent<erm::ecs::ModelComponent>();
+					ImGui::CloseCurrentPopup();
+				}
+			}
+			
+			if (!hasCamera)
+			{
+				if (ImGui::Button("Camera"))
+				{
+					game.GetECS().GetEntityById(entity)->AddComponent<erm::ecs::CameraComponent>(game.GetWindow());
+					ImGui::CloseCurrentPopup();
+				}
+			}
+			ImGui::EndPopup();
 		}
 	}
 	
