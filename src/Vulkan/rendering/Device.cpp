808ddc2737acafa8ea21a7969c678c7de1da3456
damiano.calcagni <damiano.calcagni@gmail.com>
2020-03-18 00:40:16 +0100

    Adds and uses an index buffer for rendering

--- src/Vulkan/rendering/Device.cpp
+++ src/Vulkan/rendering/Device.cpp
@@ -115,10 +115,17 @@ namespace {
 		}
 	};
 	
-	const std::vector<Vertex> vertices = {
-		{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-		{{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
-		{{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
+	const std::vector<Vertex> vertices =
+	{
+		{{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+		{{0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
+		{{0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
+		{{-0.5f, 0.5f}, {1.0f, 1.0f, 1.0f}}
+	};
+	
+	const std::vector<uint16_t> indices =
+	{
+		0, 1, 2, 2, 3, 0
 	};
 
 }
@@ -180,7 +187,11 @@ namespace erm {
 		~Impl()
 		{
 			vkDeviceWaitIdle(device);
+			
 			CleanupSwapChain();
+			
+			vkDestroyBuffer(device, indexBuffer, nullptr);
+			vkFreeMemory(device, indexBufferMemory, nullptr);
 			vkDestroyBuffer(device, vertexBuffer, nullptr);
 			vkFreeMemory(device, vertexBufferMemory, nullptr);
 			for (size_t i = 0; i < kMaxFramesInFlight; ++i)
@@ -911,6 +922,27 @@ namespace erm {
 			vkFreeMemory(device, stagingBufferMemory, nullptr);
 		}
 		
+		void CreateIndexBuffer()
+		{
+			VkDeviceSize bufferSize = sizeof(indices[0]) * indices.size();
+
+			VkBuffer stagingBuffer;
+			VkDeviceMemory stagingBufferMemory;
+			CreateBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+
+			void* data;
+			vkMapMemory(device, stagingBufferMemory, 0, bufferSize, 0, &data);
+			memcpy(data, indices.data(), (size_t) bufferSize);
+			vkUnmapMemory(device, stagingBufferMemory);
+
+			CreateBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, indexBuffer, indexBufferMemory);
+
+			CopyBuffer(stagingBuffer, indexBuffer, bufferSize);
+
+			vkDestroyBuffer(device, stagingBuffer, nullptr);
+			vkFreeMemory(device, stagingBufferMemory, nullptr);
+		}
+		
 		void CreateCommandBuffers()
 		{
 			commandBuffers.resize(swapChainFramebuffers.size());
@@ -955,7 +987,8 @@ namespace erm {
 				VkBuffer vertexBuffers[] = {vertexBuffer};
 				VkDeviceSize offsets[] = {0};
 				vkCmdBindVertexBuffers(commandBuffers[i], 0, 1, vertexBuffers, offsets);
-				vkCmdDraw(commandBuffers[i], static_cast<uint32_t>(vertices.size()), 1, 0, 0);
+				vkCmdBindIndexBuffer(commandBuffers[i], indexBuffer, 0, VK_INDEX_TYPE_UINT16);
+				vkCmdDrawIndexed(commandBuffers[i], static_cast<uint32_t>(indices.size()), 1, 0, 0, 0);
 				
 				vkCmdEndRenderPass(commandBuffers[i]);
 
@@ -1005,6 +1038,7 @@ namespace erm {
 			CreateFramebuffers();
 			CreateCommandPool();
 			CreateVertexBuffer();
+			CreateIndexBuffer();
 			CreateCommandBuffers();
 			CreateSyncObjects();
 		}
@@ -1040,7 +1074,6 @@ namespace erm {
 			CreateCommandBuffers();
 		}
 		
-	private:
 		void GetFrameBufferSize(int& width, int& height)
 		{
 			glfwGetFramebufferSize(mWindow, &width, &height);
@@ -1075,6 +1108,8 @@ namespace erm {
 		VkCommandPool commandPool;
 		VkBuffer vertexBuffer;
 		VkDeviceMemory vertexBufferMemory;
+		VkBuffer indexBuffer;
+		VkDeviceMemory indexBufferMemory;
 		std::vector<VkCommandBuffer> commandBuffers;
 		
 		std::vector<VkSemaphore> imageAvailableSemaphores;
