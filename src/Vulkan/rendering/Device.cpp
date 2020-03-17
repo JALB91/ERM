2f51e637dc0fa603ed4059d456f1000b4f27c488
damiano.calcagni <damiano.calcagni@gmail.com>
2020-03-18 00:20:14 +0100

    Implements buffer copying operation to store it in the device memory

--- src/Vulkan/rendering/Device.cpp
+++ src/Vulkan/rendering/Device.cpp
@@ -15,15 +15,6 @@
 
 namespace {
 	
-#ifndef NDEBUG
-	const std::vector<const char*> kValidationLayers
-	{
-		"VK_LAYER_KHRONOS_validation"
-	};
-	
-	VkDebugUtilsMessengerEXT debugMessenger;
-#endif
-	
 	const std::vector<const char*> kDeviceExtensions
 	{
 		VK_KHR_SWAPCHAIN_EXTENSION_NAME
@@ -32,6 +23,13 @@ namespace {
 	const int kMaxFramesInFlight = 2;
 	
 #ifndef NDEBUG
+	const std::vector<const char*> kValidationLayers
+	{
+		"VK_LAYER_KHRONOS_validation"
+	};
+	
+	VkDebugUtilsMessengerEXT debugMessenger;
+
 	VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback(
 		VkDebugUtilsMessageSeverityFlagBitsEXT /*messageSeverity*/,
 		VkDebugUtilsMessageTypeFlagsEXT /*messageType*/,
@@ -116,7 +114,7 @@ namespace {
 			return attributeDescriptions;
 		}
 	};
-
+	
 	const std::vector<Vertex> vertices = {
 		{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
 		{{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
@@ -271,6 +269,80 @@ namespace erm {
 			}
 
 			currentFrame = (currentFrame + 1) % kMaxFramesInFlight;
+		}
+		
+	private:
+		void CreateBuffer(
+			VkDeviceSize size,
+			VkBufferUsageFlags usage,
+			VkMemoryPropertyFlags properties,
+			VkBuffer& buffer,
+			VkDeviceMemory& bufferMemory
+		)
+		{
+			VkBufferCreateInfo bufferInfo = {};
+			bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+			bufferInfo.size = size;
+			bufferInfo.usage = usage;
+			bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+			if (vkCreateBuffer(device, &bufferInfo, nullptr, &buffer) != VK_SUCCESS) {
+				throw std::runtime_error("failed to create buffer!");
+			}
+
+			VkMemoryRequirements memRequirements;
+			vkGetBufferMemoryRequirements(device, buffer, &memRequirements);
+
+			VkMemoryAllocateInfo allocInfo = {};
+			allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+			allocInfo.allocationSize = memRequirements.size;
+			allocInfo.memoryTypeIndex = erm::Utils::FindMemoryType(physicalDevice, memRequirements.memoryTypeBits, properties);
+
+			if (vkAllocateMemory(device, &allocInfo, nullptr, &bufferMemory) != VK_SUCCESS) {
+				throw std::runtime_error("failed to allocate buffer memory!");
+			}
+
+			vkBindBufferMemory(device, buffer, bufferMemory, 0);
+		}
+		
+		void CopyBuffer(
+			VkBuffer srcBuffer,
+			VkBuffer dstBuffer,
+			VkDeviceSize size
+		)
+		{
+			VkCommandBufferAllocateInfo allocInfo = {};
+			allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+			allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+			allocInfo.commandPool = commandPool;
+			allocInfo.commandBufferCount = 1;
+
+			VkCommandBuffer commandBuffer;
+			vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer);
+			
+			VkCommandBufferBeginInfo beginInfo = {};
+			beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+			beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+			vkBeginCommandBuffer(commandBuffer, &beginInfo);
+			
+			VkBufferCopy copyRegion = {};
+			copyRegion.srcOffset = 0; // Optional
+			copyRegion.dstOffset = 0; // Optional
+			copyRegion.size = size;
+			vkCmdCopyBuffer(commandBuffer, srcBuffer, dstBuffer, 1, &copyRegion);
+			
+			vkEndCommandBuffer(commandBuffer);
+			
+			VkSubmitInfo submitInfo = {};
+			submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+			submitInfo.commandBufferCount = 1;
+			submitInfo.pCommandBuffers = &commandBuffer;
+
+			vkQueueSubmit(graphicsQueue, 1, &submitInfo, VK_NULL_HANDLE);
+			vkQueueWaitIdle(graphicsQueue);
+			
+			vkFreeCommandBuffers(device, commandPool, 1, &commandBuffer);
 		}
 		
 		void CreateInstance()
@@ -808,40 +880,35 @@ namespace erm {
 		
 		void CreateVertexBuffer()
 		{
-			VkBufferCreateInfo bufferInfo = {};
-			bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-			bufferInfo.size = sizeof(vertices[0]) * vertices.size();
-			bufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
-			bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+			VkDeviceSize bufferSize = sizeof(vertices[0]) * vertices.size();
 
-			if (vkCreateBuffer(device, &bufferInfo, nullptr, &vertexBuffer) != VK_SUCCESS)
-			{
-				throw std::runtime_error("Failed to create vertex buffer!");
-			}
+			VkBuffer stagingBuffer;
+			VkDeviceMemory stagingBufferMemory;
+			CreateBuffer(
+				bufferSize,
+				VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+				VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+				stagingBuffer,
+				stagingBufferMemory
+			);
 
-			VkMemoryRequirements memRequirements;
-			vkGetBufferMemoryRequirements(device, vertexBuffer, &memRequirements);
-			
-			VkMemoryAllocateInfo allocInfo = {};
-			allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
-			allocInfo.allocationSize = memRequirements.size;
-			allocInfo.memoryTypeIndex = erm::Utils::FindMemoryType(
-				physicalDevice,
-				memRequirements.memoryTypeBits,
-				VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT
+			void* data;
+			vkMapMemory(device, stagingBufferMemory, 0, bufferSize, 0, &data);
+			memcpy(data, vertices.data(), (size_t) bufferSize);
+			vkUnmapMemory(device, stagingBufferMemory);
+
+			CreateBuffer(
+				bufferSize,
+				VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+				VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+				vertexBuffer,
+				vertexBufferMemory
 			);
 			
-			if (vkAllocateMemory(device, &allocInfo, nullptr, &vertexBufferMemory) != VK_SUCCESS)
-			{
-				throw std::runtime_error("failed to allocate vertex buffer memory!");
-			}
+			CopyBuffer(stagingBuffer, vertexBuffer, bufferSize);
 			
-			vkBindBufferMemory(device, vertexBuffer, vertexBufferMemory, 0);
-			
-			void* data;
-			vkMapMemory(device, vertexBufferMemory, 0, bufferInfo.size, 0, &data);
-			memcpy(data, vertices.data(), static_cast<size_t>(bufferInfo.size));
-			vkUnmapMemory(device, vertexBufferMemory);
+			vkDestroyBuffer(device, stagingBuffer, nullptr);
+			vkFreeMemory(device, stagingBufferMemory, nullptr);
 		}
 		
 		void CreateCommandBuffers()
