a83939df7b7d77c592a4416f687d32e64a69c305
damiano.calcagni <damiano.calcagni@gmail.com>
2021-01-22 21:28:14 +0100

    Minor improvements to the clang formatting script

--- cl-fmt.sh
+++ cl-fmt.sh
@@ -15,25 +15,32 @@ for clangfmt in clang-format{,-{4,3}.{9,8,7,6,5,4,3,2,1,0}}; do
 done
 
 # Check if we found a working clang-format
-if [ -z "$FMT" ]; then
+if [[ -z "$FMT" ]]; then
     echo "failed to find clang-format"
     exit 1
 fi
 
-function format() {
+function format_file() {
+	echo "Formatting file ${1}"
+	${FMT} -i ${1}
+}
+
+function format_dir() {
     for f in $(find $@ -name '*.h' -or -name '*.m' -or -name '*.mm' -or -name '*.c' -or -name '*.cpp'); do 
-        echo "format ${f}";
-        ${FMT} -i ${f};
+		format_file ${f}
     done
 
-    echo "~~~ $@ Done ~~~";
 }
 
 # Check all of the arguments first to make sure they're all directories
-for dir in "$@"; do
-    if [ ! -d "${dir}" ]; then
-        echo "${dir} is not a directory";
-    else
-        format ${dir};
+for path in "$@"; do
+    if [[ -d "${path}" ]]; then
+        format_dir ${path};
+	elif [[ -f "${path}" ]]; then
+		format_file ${path}
+	else
+		echo "The path provided is not valid: ${path}"
     fi
+
+    echo "~~~ Done ~~~";
 done
