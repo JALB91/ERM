f2b2330b013226b387b2222fe30696e685e7b6ed
Damiano Calcagni <damiano.calcagni@gmail.com>
2021-03-31 22:49:58 +0200

    Removes unused method of resource pipeline script

--- scripts/resources_pipeline.py
+++ scripts/resources_pipeline.py
@@ -1,30 +1,5 @@
 import sys, argparse, shutil, os
 
-def update_asset(src_folder, dest_folder, subdir, asset):
-    dest_path = os.path.join(dest_folder, subdir, asset)
-    if "_tmp" in asset:
-        os.remove(dest_path)
-        return
-    src_path = os.path.join(src_folder, subdir, asset)
-    if src_path.endswith(".cmp"):
-        src_path = src_path[:-4]
-    if not os.path.exists(src_path):
-        os.remove(dest_path)
-        return
-    src_time = os.path.getmtime(src_path)
-    dest_time = os.path.getmtime(dest_path)
-    if not os.path.exists(src_path) or src_time > dest_time:
-        os.remove(dest_path)
-
-def update_resources(src_folder, dest_folder):
-    for root, subdir, files in os.walk(dest_folder):
-        for asset in files:
-            subdir = ""
-            if len(root) > len(dest_folder):
-                subdir = root[len(dest_folder) + 1:]
-            update_asset(src_folder, dest_folder, subdir, asset)
-    shutil.copytree(src_folder, dest_folder)
-
 def compile_shaders(shaders_compiler, res_folder):
     for root, subdir, files in os.walk(os.path.join(res_folder, "shaders")):
         for shader in files:
@@ -49,7 +24,7 @@ if __name__ == "__main__":
     except:
         print("Failed to delete the resources destination folder")
     
-    update_resources(args.res_src, args.res_dest)
+    shutil.copytree(args.res_src, args.res_dest)
 
     if (args.shaders_compiler):
         compile_shaders(args.shaders_compiler, args.res_dest)
